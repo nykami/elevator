77e253a14eb523aab28193f6140f33cd63017254
nykami <kamilla.nyitrai@yahoo.com>
2023-06-03 01:18:40 +0300

    New layout

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
 const openElevatorDoor = new Image();
 const closedElevatorDoor = new Image();
 openElevatorDoor.src = 'images/openElevatorDoor.png';
@@ -11,25 +9,20 @@ const paddingBottom = 0.5;
 const paddingTop = 6;
 
 let xA = paddingHorizontal;
-let yA = canvas.height - elevatorHeight ;
-let xB = canvas.width - elevatorWidth - paddingHorizontal;
 let yB = paddingTop;
 
 const upButtons = Array.from(document.getElementsByClassName('up-button'));
 const downButtons = Array.from(document.getElementsByClassName('down-button'));
 const allButtons = upButtons.concat(downButtons);
-
-function drawElevators() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(openElevatorDoor, xA, yA, elevatorWidth, elevatorHeight);
-  context.drawImage(closedElevatorDoor, xB, yB, elevatorWidth, elevatorHeight);
-}
+const floorButtons = Array.from(document.getElementsByClassName('lift-floor-button'));
 
 window.onload = () => {
-  drawElevators();
-  //drawElevator(elevatorB, xB, yB)
-
   allButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.style.borderColor = 'green';
+    })
+  });
+  floorButtons.forEach(button => {
     button.addEventListener('click', () => {
       button.style.borderColor = 'green';
     })
