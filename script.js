5a818273a4c548b5768fc074f21b8ffe68bb0d26
nykami <kamilla.nyitrai@yahoo.com>
2023-06-03 18:19:00 +0300

    Move elevators when called

--- script.js
+++ script.js
@@ -1,113 +1,157 @@
 const numOfFloors = 7;
+const doorWidth = 100;
+const doorHeight = 150;
 const upButtons = document.querySelectorAll('.up-button');
 const downButtons = document.querySelectorAll('.down-button');
-
-
+const floorHeight = 200;
 const elevatorA = document.getElementById('movingElevatorA');
-const floorHeight = elevatorA.parentElement.parentElement.offsetHeight;
 const elevatorB = document.getElementById('movingElevatorB');
+let currentFloorA = 1;
+let currentFloorB = 7;
 
-function getCurrentFloor(elevator) {
-  const translateY = parseInt(window.getComputedStyle(elevator).getPropertyValue('transform').split(',')[5]);
-  const currentFloor = Math.abs(Math.round(translateY / floorHeight)) + 1;
-  return currentFloor;
-}
-
-// Function to animate the elevator to the target floor
-function animateElevator(elevator, currentFloor, targetFloor, duration) {
-  let direction = null;
+function moveElevator(elevator, currentFloor, targetFloor, duration, distance) {
   let targetTranslateY = null;
+
   if (currentFloor < targetFloor) {
     // moving upwards
-    direction = 1;
-    console.log("targetFloor: ", targetFloor);
-    console.log("floorHeight: ", floorHeight);
-    targetTranslateY = -((targetFloor) * floorHeight) + floorHeight - (10 * (targetFloor - 1));
-    console.log("targetTranslateY: ", targetTranslateY);
+    switch (elevator) {
+      case elevatorA: {
+        targetTranslateY = -((currentFloor - 1) * floorHeight) - (distance * floorHeight);
+        console.log(`Elevator A moving to floor ${targetFloor - 1}`);
+        break;
+      }
+      case elevatorB: {
+        targetTranslateY = ((numOfFloors - currentFloor) * floorHeight) - (distance * floorHeight);
+        console.log(`Elevator B moving to floor ${targetFloor - 1}`);
+        break;
+      }
+    }
   }
   else {
     // moving downwards
-    direction = -1;
-    console.log("targetFloor: ", targetFloor);
-    console.log("floorHeight: ", floorHeight);
-    targetTranslateY = ((targetFloor - 1) * floorHeight) + (10 * (currentFloor - targetFloor)) - floorHeight; console.log("targetTranslateY: ", targetTranslateY);
+    switch (elevator) {
+      case elevatorA: {
+        targetTranslateY = (distance * floorHeight) - ((currentFloor - 1) * floorHeight);
+        console.log(`Elevator A moving to floor ${targetFloor - 1}`);
+        break;
+      }
+      case elevatorB: {
+        targetTranslateY = (distance * floorHeight) + ((numOfFloors - currentFloor) * floorHeight);
+        console.log(`Elevator B moving to floor ${targetFloor - 1}`);
+        break;
+      }
+    }
   }
-
-  // Apply CSS animation to elevator
   elevator.style.transition = `transform ${duration / 1000}s ease-in-out`;
   elevator.style.transform = `translateY(${targetTranslateY}px)`;
 }
 
 window.onload = () => {
-  // Add event listener to each floor button
   upButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      // Add green border to the clicked button
-      button.style.borderColor = 'green';
-      // Calculate the distance to the target floor
-      const targetFloor = numOfFloors - index;
-      let currentFloorA = getCurrentFloor(elevatorA);
-      if (isNaN(currentFloorA)) {
-        currentFloorA = 1;
-      }
-      let currentFloorB = getCurrentFloor(elevatorB);
-      if (isNaN(currentFloorB)) {
-        currentFloorB = 7;
-      }
+      let targetFloor = numOfFloors - index;
 
       const distanceA = Math.abs(targetFloor - currentFloorA);
       const distanceB = Math.abs(targetFloor - currentFloorB);
 
-      let chosenElevator = null;
-      let distance = null;
-      let currentFloor = null;
-      if (distanceA <= distanceB) {
-        chosenElevator = elevatorA;
-        distance = distanceA;
-        currentFloor = currentFloorA;
-      } else {
-        chosenElevator = elevatorB;
-        distance = distanceB;
-        currentFloor = currentFloorB;
+      if (distanceA == 0 || distanceB == 0) {
+        alert(`There is already one elevator on floor ${targetFloor - 1} `);
       }
+      else {
+        button.style.borderColor = 'green';
+        if (currentFloorA < currentFloorB) { // case elevatorA is on lower level than elevatorB
+          if (distanceA <= distanceB) {
 
-      // Calculate the duration for the elevator to reach the target floor (assuming 1 floor per second)
-      const duration = distance * 1000;
-
-      // Animate the elevator to the target floor
-      console.log(`Elevator ${chosenElevator} moving to floor ${targetFloor}`);
-      animateElevator(chosenElevator, currentFloor, targetFloor, duration);
-
-      // Reset the button border color after the elevator reaches the target floor
-      setTimeout(() => {
-        console.log(`Elevator ${chosenElevator} reached ${targetFloor - 1} floor`);
-        button.style.borderColor = '';
-      }, duration);
+            const duration = distanceA * 1000;
+            moveElevator(elevatorA, currentFloorA, targetFloor, duration, distanceA);
+            currentFloorA = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator A reached floor ${targetFloor - 1} `);
+              button.style.borderColor = '';
+            }, duration);
+          } else {
+            const duration = distanceB * 1000;
+            moveElevator(elevatorB, currentFloorB, targetFloor, duration, distanceB);
+            currentFloorB = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator B reached floor ${targetFloor - 1}`);
+              button.style.borderColor = '';
+            }, duration);
+          }
+        } else if (currentFloorA > currentFloorB) {  // case elevatorA is on higher floor than elevatorB
+          if (distanceA < distanceB) {
+            const duration = distanceA * 1000;
+            moveElevator(elevatorA, currentFloorA, targetFloor, duration, distanceA);
+            currentFloorA = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator A reached floor ${targetFloor - 1}`);
+              button.style.borderColor = '';
+            }, duration);
+          } else {
+            const duration = distanceB * 1000;
+            moveElevator(elevatorB, currentFloorB, targetFloor, duration, distanceB);
+            currentFloorB = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator B reached floor ${targetFloor - 1} `);
+              button.style.borderColor = '';
+            }, duration);
+          }
+        }
+      }
     });
   });
 
-  // Add event listener to each floor button
   downButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      // Add green border to the clicked button
-      button.style.borderColor = 'green';
-      // Calculate the distance to the target floor
-      const targetFloor = numOfFloors - index;
-      const currentFloor = getCurrentFloor(elevatorA);
-      const distance = Math.abs(targetFloor - currentFloor);
+      let targetFloor = numOfFloors - index;
 
-      // Calculate the duration for the elevator to reach the target floor (assuming 1 floor per second)
-      const duration = distance * 1000;
+      const distanceA = Math.abs(targetFloor - currentFloorA);
+      const distanceB = Math.abs(targetFloor - currentFloorB);
 
-      // Animate the elevator to the target floor
-      console.log("Eevator moving down");
-      animateElevator(elevatorA, targetFloor, duration);
+      if (distanceA == 0 || distanceB == 0) {
+        alert(`There is already one elevator on floor ${targetFloor - 1} `);
+      }
+      else {
+        button.style.borderColor = 'green';
+        if (currentFloorA < currentFloorB) { // case elevatorA is on lower level than elevatorB
+          if (distanceA <= distanceB) {
 
-      // Reset the button border color after the elevator reaches the target floor
-      setTimeout(() => {
-        console.log(`Elevator reached ${targetFloor - 1} floor`);
-        button.style.borderColor = '';
-      }, duration);
+            const duration = distanceA * 1000;
+            moveElevator(elevatorA, currentFloorA, targetFloor, duration, distanceA);
+            currentFloorA = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator A reached floor ${targetFloor - 1} `);
+              button.style.borderColor = '';
+            }, duration);
+          } else {
+            const duration = distanceB * 1000;
+            moveElevator(elevatorB, currentFloorB, targetFloor, duration, distanceB);
+            currentFloorB = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator B reached floor ${targetFloor - 1}`);
+              button.style.borderColor = '';
+            }, duration);
+          }
+        } else if (currentFloorA > currentFloorB) {  // case elevatorA is on higher floor than elevatorB
+          if (distanceA < distanceB) {
+            const duration = distanceA * 1000;
+            moveElevator(elevatorA, currentFloorA, targetFloor, duration, distanceA);
+            currentFloorA = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator A reached floor ${targetFloor - 1}`);
+              button.style.borderColor = '';
+            }, duration);
+          } else {
+            const duration = distanceB * 1000;
+            moveElevator(elevatorB, currentFloorB, targetFloor, duration, distanceB);
+            currentFloorB = targetFloor;
+            setTimeout(() => {
+              console.log(`Elevator B reached floor ${targetFloor - 1} `);
+              button.style.borderColor = '';
+            }, duration);
+          }
+        }
+      }
     });
   });
 };
