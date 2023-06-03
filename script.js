87487e25f9b87878960a1afbdf3f09b3b299f9ea
nykami <kamilla.nyitrai@yahoo.com>
2023-06-03 03:57:34 +0300

    One elevator movement

--- script.js
+++ script.js
@@ -1,32 +1,77 @@
-const openElevatorDoor = new Image();
-const closedElevatorDoor = new Image();
-openElevatorDoor.src = 'images/openElevatorDoor.png';
-closedElevatorDoor.src = 'images/closedElevatorDoor.png';
-const elevatorWidth = 40;
-const elevatorHeight = 15;
-const paddingHorizontal = 40;
-const paddingBottom = 0.5;
-const paddingTop = 6;
+const numOfFloors = 7;
+const upButtons = document.querySelectorAll('.up-button');
+const downButtons = document.querySelectorAll('.down-button');
 
-let xA = paddingHorizontal;
-let yB = paddingTop;
 
-const upButtons = Array.from(document.getElementsByClassName('up-button'));
-const downButtons = Array.from(document.getElementsByClassName('down-button'));
-const allButtons = upButtons.concat(downButtons);
-const floorButtons = Array.from(document.getElementsByClassName('lift-floor-button'));
+const elevatorA = document.getElementById('movingElevatorA');
+const floorHeight = elevatorA.parentElement.parentElement.offsetHeight;
+const elevatorB = document.getElementById('movingElevatorB');
+
+function getCurrentFloor(elevator) {
+  const translateY = parseInt(window.getComputedStyle(elevator).getPropertyValue('transform').split(',')[5]);
+  const currentFloor = Math.abs(Math.round(translateY / floorHeight)) + 1;
+  return currentFloor;
+}
+
+// Function to animate the elevator to the target floor
+function animateElevator(elevator, targetFloor, duration) {
+  const targetTranslateY = -((targetFloor) * floorHeight) + floorHeight - 10 * (targetFloor - 1);
+  console.log("targetTranslateY: ", targetTranslateY);
+  // Apply CSS animation to elevator
+  elevator.style.transition = `transform ${duration / 1000}s ease-in-out`;
+  elevator.style.transform = `translateY(${targetTranslateY}px)`;
+}
 
 window.onload = () => {
-  allButtons.forEach(button => {
+  // Add event listener to each floor button
+  upButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
+      // Add green border to the clicked button
       button.style.borderColor = 'green';
-    })
+      console.log("Button index: ", index);
+      // Calculate the distance to the target floor
+      const targetFloor = numOfFloors - index;
+      const currentFloor = getCurrentFloor(elevatorA);
+      console.log('currentFloor: ', currentFloor);
+      console.log('target: ', targetFloor);
+      const distance = Math.abs(targetFloor - currentFloor);
+
+      // Calculate the duration for the elevator to reach the target floor (assuming 1 floor per second)
+      const duration = distance * 1000;
+
+      // Animate the elevator to the target floor
+      animateElevator(elevatorA, targetFloor, duration);
+
+      // Reset the button border color after the elevator reaches the target floor
+      setTimeout(() => {
+        button.style.borderColor = '';
+      }, duration);
+    });
   });
-  floorButtons.forEach(button => {
+
+  downButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
+      // Add green border to the clicked button
       button.style.borderColor = 'green';
-    })
+
+      // Calculate the distance to the target floor
+      const targetFloor = numOfFloors - index;
+      const currentFloor = getCurrentFloor(elevatorA);
+      console.log('current: ', currentFloor);
+      // console.log('target: ', targetFloor);
+      const distance = Math.abs(targetFloor - currentFloor);
+
+      // Calculate the duration for the elevator to reach the target floor (assuming 1 floor per second)
+      const duration = distance * 1000;
+
+      // Animate the elevator to the target floor
+      animateElevator(elevatorA, numOfFloors - targetFloor, duration);
+
+      // Reset the button border color after the elevator reaches the target floor
+      setTimeout(() => {
+        button.style.borderColor = '';
+      }, duration);
+    });
   });
 };
-
 
